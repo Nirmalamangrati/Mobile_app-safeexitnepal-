2536d4ea06806e01f8ba5828771873ae27cc8e7c
Nirmala Mangrati <nirmalamgrt@gmail.com>
2026-06-23 11:17:13 +0545

    finished  weather app

--- app/index.tsx
+++ app/index.tsx
@@ -2,7 +2,6 @@ import { Redirect } from "expo-router";
 import React from "react";
 export default function Index() {
   const isUserLoggedIn = false;
-
   if (isUserLoggedIn) {
     return <Redirect href="/(tabs)/home" />;
   } else {
