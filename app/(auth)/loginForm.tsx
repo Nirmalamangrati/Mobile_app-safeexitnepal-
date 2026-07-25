99d08d0749a7c74cf45f467149db3ba4c84902bd
Nirmala Mangrati <nirmalamgrt@gmail.com>
2026-07-25 18:46:58 +0545

    fixed nodemailer

--- app/(auth)/loginForm.tsx
+++ app/(auth)/loginForm.tsx
@@ -67,10 +67,6 @@ const LoginForm = () => {
         });
         const data = await response.json();
         if (response.ok) {
-          // Alert.alert(
-          //   "Success",
-          //   "OTP has been sent to your contact. Please check.",
-          // );
           router.push({
             pathname: "/(auth)/verifyOtp",
             params: { contact: contact },
